9b9f8e418f26ba430965424e88319917f7eaa8c7
Viktor Elofsson <viktor@viktorelofsson.se>
2019-11-10 15:05:38 +0100

    Update for Porla 0.14

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const { promisify } = require('util');
 const readdir       = promisify(fs.readdir);
 const unlink        = promisify(fs.unlink);
 
+const md5 = (input) => crypto.createHash('md5')
+                             .update(input)
+                             .digest('hex');
+
 module.exports = (watchPath, options) => {
     options = options || {};
 
@@ -18,9 +22,7 @@ module.exports = (watchPath, options) => {
 
     // Create a hash of the path specified and use as a key for the
     // data storage. This ensures no strange paths in the KVS keys.
-    const hashedPath = crypto.createHash('md5')
-                             .update(watchPath)
-                             .digest('hex');
+    const hashedPath = md5(watchPath);
 
     return async (porla) => {
         const history = await porla.config.get(`autoadd.${hashedPath}.history`) || [];
@@ -31,7 +33,12 @@ module.exports = (watchPath, options) => {
                 const files = await readdir(watchPath);
 
                 for (const file of files) {
-                    if (history.includes(file)) {
+                    // We store the md5 hash of the file name in our history state
+                    // to avoid storing potentially sensitive file names.
+
+                    const fileHash = md5(file);
+
+                    if (history.includes(fileHash)) {
                         continue;
                     }
 
@@ -57,7 +64,7 @@ module.exports = (watchPath, options) => {
                             porla.log.error(`autoadd: Failed to remove file: ${err}`);
                         }
                     } else {
-                        history.push(file);
+                        history.push(fileHash);
                     }
                 }
 
@@ -72,7 +79,7 @@ module.exports = (watchPath, options) => {
         porla.on('shutdown', async () => {
             clearTimeout(timer);
 
-            porla.log.info(`autoadd: Saving history (${history.length} items)`);
+            porla.log.info('autoadd: Saving history for %d item(s)', history.length);
 
             await porla.config.set(`autoadd.${hashedPath}.history`, history);
         });
